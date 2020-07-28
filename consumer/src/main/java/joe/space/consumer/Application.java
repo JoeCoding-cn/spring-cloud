ee62d90ace73dd246e5de2c2b3dceb46e895c7e9
joejay <1815066657@qq.com>
2020-07-28 16:54:55 +0800

    init project and serverRegister

---
+++ consumer/src/main/java/joe/space/consumer/Application.java
@@ -0,0 +1,19 @@
+package joe.space.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Joe
+ * createTime 2020/07/24 09:50
+ * mail joe-code@foxmail.com
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ producer/src/main/java/joe/space/producer/Application.java
@@ -0,0 +1,19 @@
+package joe.space.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Joe
+ * createTime 2020/07/23 17:55
+ * mail joe-code@foxmail.com
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
